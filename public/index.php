35296565167064da5baa5c754a235ab47eb9c500
Andri Syahputra <65515899+andrisyahputra@users.noreply.github.com>
2023-07-31 12:42:35 +0700

    fix include

--- public/index.php
+++ public/index.php
@@ -1,19 +1,44 @@
 <?php
+// session_start();
+// // include function
+// require '../private/include/function.php';
+
+// $page = isset($_GET['url']) ? $_GET['url'] : 'home';
+
+// // INI ADALAH FOLDER NYA
+// $folder = '../private/include/';
+
+// // UNTUK MENDAPATKAN SEMUA FILES
+// $files = glob($folder . "*.php");
+// $file_name = $folder . $page . ".php";
+// // var_dump($files);
+// if (in_array($file_name, $files)) {
+//     include "$file_name";
+// } else {
+//     include "../private/include/404.php";
+// }
 session_start();
-// include function
 require '../private/include/function.php';
 
+// Daftar putih (whitelist) file yang diperbolehkan
+$allowedFiles = array(
+    'home' => 'home.php',
+    'about' => 'about.php',
+    'contact' => 'contact.php',
+);
+
+// Mendapatkan nilai dari parameter 'url' dan membersihkannya
 $page = isset($_GET['url']) ? $_GET['url'] : 'home';
+$page = basename($page); // Hanya mengambil bagian paling akhir dari jalur file, jika ada
 
-// INI ADALAH FOLDER NYA
-$folder = '../private/include/';
-
-// UNTUK MENDAPATKAN SEMUA FILES
-$files = glob($folder . "*.php");
-$file_name = $folder . $page . ".php";
-// var_dump($files);
-if (in_array($file_name, $files)) {
-    include "$file_name";
+// Cek apakah nilai $page ada dalam daftar putih
+if (isset($allowedFiles[$page])) {
+    $file_name = '../private/include/' . $allowedFiles[$page];
+    if (file_exists($file_name)) {
+        include $file_name;
+    } else {
+        include '../private/include/404.php';
+    }
 } else {
-    include "../private/include/404.php";
+    include '../private/include/404.php';
 }
